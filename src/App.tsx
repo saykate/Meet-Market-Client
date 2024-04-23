945556a6ebfcdaf1826685293637d81bbd4a4ff0
Katie Sayers <kate@student.dev-island.org>
2024-04-23 13:15:52 -1000

    added env

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 // import { useState } from 'react'
+import { useEffect } from 'react'
 import './App.css'
+const URL = import.meta.env.VITE_SERVER_URL;
 
 function App() {
   // const [count, setCount] = useState(0)
 
+  const hitServer = async() => {
+    const resp = await fetch(URL)
+    const json = await resp.json()
+    console.log(json)
+  }
+
+useEffect(() => {
+  hitServer()
+}, [])
+
   return (
     <>
-    
+      <p>Hey there</p>
     </>
   )
 }
